0627bae39c4361b12fcde09deb723d74144e0a7f
Aymen Turki <aymen.turki.pro@gmail.com>
2020-01-09 17:58:29 +0100

    Removing block comment from block comment

--- ELearningAPI/Controllers/AuthController.cs
+++ ELearningAPI/Controllers/AuthController.cs
@@ -98,13 +98,13 @@ namespace ELearningAPI.Controllers
         {
             return Ok(new { res = "good" });
 
-            /*return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
 			{
 				Date = DateTime.Now.AddDays(index),
 				TemperatureC = rng.Next(-20, 55),
 				Summary = Summaries[rng.Next(Summaries.Length)]
 			})
-			.ToArray();*/
+			.ToArray();
         }
 
         [HttpPost, Authorize(Roles = "Learner"), Route("test2")]
